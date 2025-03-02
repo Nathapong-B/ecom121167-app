444f65f095382e6b37a05576e87202b6d00c850a
PoPKuB <btf_01@hotmail.com>
2025-03-02 14:55:26 +0700

    v2

--- src/pages/homepage/components/header.jsx
+++ src/pages/homepage/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <div className="relative h-max w-full">
             <div className="absolute -top-10 bg-gray-300 h-screen w-full flex justify-center items-center">
-                <div className="text-4xl font-bold">Header Section</div>
+                <div className="text-4xl font-bold">Header Section 001</div>
             </div>
         </div>
     )
