9ae8c6b99bdb8752c4c1e98a5845b258bd53770a
PoPKuB <btf_01@hotmail.com>
2025-03-05 13:07:53 +0700

    fix bug formproduct

--- src/pages/backoffice/components/products/formProduct.jsx
+++ src/pages/backoffice/components/products/formProduct.jsx
@@ -22,13 +22,13 @@ export default function FormProduct(props) {
         <div>
             <form className="my-4">
                 <div className="relative flex items-center py-4">
-                    <button name="product_name" className={data?.product_name ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Product name</button>
-                    <input name="product_name" value={data?.product_name ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-full" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
+                    <label name="product_name" id="label_product_name" htmlFor="product_name" className={data?.product_name ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Product name</label>
+                    <input name="product_name" id="product_name" value={data?.product_name ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-full" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
                 </div>
 
                 <div className="relative flex items-center py-4">
-                    <button name="category_id" className={data?.category_id ? `label-input-animate z-20 ${cssTopNag}` : `label-input-animate z-0`} onClick={(e) => hdlClickInput(e)}>Category</button>
-                    <select name="category_id" value={data?.category_id ?? ''} onChange={e => hdlInputChange(e)} className="frm-input z-10" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}>
+                    <label name="category_id" id="label_category_id" htmlFor="category_id" className={data?.category_id ? `label-input-animate z-20 ${cssTopNag}` : `label-input-animate z-0`} onClick={(e) => hdlClickInput(e)}>Category</label>
+                    <select name="category_id" id="category_id" value={data?.category_id ?? ''} onChange={e => hdlInputChange(e)} className="frm-input z-10" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}>
                         <option value='' disabled>Select Categories</option>
                         {categories
                             ?
@@ -40,24 +40,24 @@ export default function FormProduct(props) {
 
                 <div className="flex">
                     <div className="relative flex items-center py-4 w-1/3">
-                        <button name="stock" className={data?.stock ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Stock</button>
-                        <input name="stock" value={data?.stock ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
+                        <label id="label_stock" name="stock" htmlFor="stock" className={data?.stock ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Stock</label>
+                        <input id="stock" name="stock" value={data?.stock ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
                     </div>
 
                     <div className="relative flex items-center py-4 w-1/3">
-                        <button name="cost" className={data?.cost ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Cost</button>
-                        <input name="cost" value={data?.cost ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
+                        <label id="label_cost" name="cost" htmlFor="cost" className={data?.cost ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Cost</label>
+                        <input id="cost" name="cost" value={data?.cost ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
                     </div>
 
                     <div className="relative flex items-center py-4 w-1/3">
-                        <button name="price" className={data?.price ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Price</button>
-                        <input name="price" value={data?.price ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
+                        <label id="label_price" htmlFor="price" name="price" className={data?.price ? `label-input-animate ${cssTopNag}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Price</label>
+                        <input id="price" name="price" value={data?.price ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-2/3" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)}></input>
                     </div>
                 </div>
 
                 <div className="relative py-4">
-                    <button name="description" className={data?.description ? `label-input-animate ${cssTopNagDes}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Description</button>
-                    <textarea name="description" value={data?.description ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-full" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)} ></textarea>
+                    <label id="label_description" htmlFor="description" name="description" className={data?.description ? `label-input-animate ${cssTopNagDes}` : `label-input-animate`} onClick={(e) => hdlClickInput(e)}>Description</label>
+                    <textarea id="description" name="description" value={data?.description ?? ''} onChange={e => hdlInputChange(e)} className="frm-input w-full" onClick={(e) => hdlClickInput(e)} onBlur={e => hdlInputOnBlur(e)} ></textarea>
                 </div>
             </form >
 
