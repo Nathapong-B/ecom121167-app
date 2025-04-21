ded605e3252af446e4ffa5f0db69548e8eeb0a6c
PoPKuB <btf_01@hotmail.com>
2025-04-21 11:37:59 +0700

    fix responsive header

--- src/pages/homepage/components/header.jsx
+++ src/pages/homepage/components/header.jsx
@@ -4,14 +4,13 @@ export default function Header() {
             <div className="relative -top-10 bg-gray-900 h-screen w-full">
                 <div className="relative max-w-6xl h-full flex justify-start items-center mx-auto">
 
-                    <div className="absolute bg-gradient-to-r from-gray-100 to-gray-100/0 md:from-gray-100/0 md:statice w-1/2 text-4xl font-black text-center text-gray-200">
+                    <div className="absolute bg-gradient-to-r from-gray-100 to-gray-100/0 md:from-gray-100/0 md:statice w-1/2 text-4xl font-black text-center text-gray-200 z-20">
 
-                        <div className="flex justify-center">
+                        <div className="flex justify-center min-w-max">
 
                             <div className="bg-red-500 relative">
                                 <span className="absolute -top-[65px] italic text-9xl text-gray-500">e</span>
                             </div>
-
                             <div className="ps-[60px]">
                                 <span className="me-2 text-yellow-400">-com</span>
                                 <span>SHOP</span>
@@ -20,12 +19,9 @@ export default function Header() {
                         </div>
 
                         <div className="text-gray-500 text-xs">Shop review.</div>
-
                     </div>
 
-
-
-                    <div className="mx-auto md:absolute md:top-10 md:-right-0">
+                    <div className="mx-auto md:absolute md:top-10 md:-right-0 z-10">
                         <img src="/pngegg.png" className="w-[650px]"></img>
                     </div>
 
